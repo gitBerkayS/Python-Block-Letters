73e4cbb581f25667cd61a04d02e7811e892de650
Berkay Sefayi <themert24seven@gmail.com>
2023-12-23 13:25:18 -0800

    Add files via upload

---
+++ BlockArt.py
@@ -0,0 +1,12 @@
+#Creating A Python Block Letter Art
+
+initial_b = "B"
+initial_s = "S"
+
+print(initial_b, initial_b, initial_b, initial_b, " " , " ", " ", " ", initial_s, initial_s, initial_s, " " )
+print(initial_b, " ", " ", " ", initial_b,  " ", " ", initial_s, " ", " ", " ",  initial_s)
+print(initial_b, " ", " ", " ", initial_b, " ", " ", initial_s, " ", " ", " ", " ")
+print(initial_b, initial_b, initial_b, initial_b, " ", " ", " ", " ", initial_s, initial_s, initial_s, " " )
+print(initial_b, " ", " ", " ", initial_b, " ", " ", " ", " ", " ", " ",  initial_s)
+print(initial_b, " ", " ", " ", initial_b, " ", " ", initial_s, " ", " ", " ",  initial_s)
+print(initial_b, initial_b, initial_b, initial_b, " ", " ", " ", " ", initial_s, initial_s, initial_s, " " )
